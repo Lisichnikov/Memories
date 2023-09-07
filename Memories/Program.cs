56c8f41c4a5315ce45b0e7496abecbc528f9ad30
alexo <alexo@DESKTOP-KEP4AP2>
2023-09-07 11:32:15 +0300

    Добавьте файлы проекта.

---
+++ Memories/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Привет чувачок");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int result = 0;
+for (int i = 0; i < Math.Abs(x); i++)
+{
+    result += Math.Abs(y);
+}
+if (x > 0 && y > 0)
+    Console.WriteLine(x + " * " + y + " = " + result);
+if (x < 0 && y < 0)
+    Console.WriteLine(x + " * " + y + " = " + result);
+if (x > 0 && y < 0) 
+    Console.WriteLine(x + " * " + y + " = " + "-" + result);
+if (x < 0 && y > 0)
+    Console.WriteLine(x + " * " + y + " = " + "-" + result);
